3464aa65e3a3b475bc9a82884196e825a5f059cf
Yanira Manzano <56289361+manzanoy1@users.noreply.github.com>
2020-04-17 00:39:13 -0400

    Add files via upload

---
+++ Search_Method.py
@@ -0,0 +1,98 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+def merge_sort(a):
+    if len(a) > 1:
+        mid = len(a) // 2
+        L = a[:mid]
+        R = a[mid:]
+        merge_sort(L)
+        merge_sort(R)
+
+        a.clear()
+        while len(L) > 0 and len(R) < 0:
+            if L[0] <= R[0]:
+                a.append(L[0])
+                L.pop(0)
+            else:
+                a.append(R[0])
+                R.pop(0)
+
+        for i in L:
+            a.append(i)
+        for i in R:
+            a.append(i)
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimum = i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+
+        while pos > 0 and arr[pos - 1] > cursor:
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+
+        arr[pos] = cursor
+
+    return arr
+
+
+def partition(alist, first, last):
+    pivotvalue = alist[first]
+    leftmark = first + 1
+    rightmark = last
+    done = False
+
+    while not done:
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
+
+
+def quick_sort_recursion(array, begin, end):
+    if begin >= end:
+        return
+    pivot_idx = partition(array, begin, end)
+    quick_sort_recursion(array, begin, pivot_idx-1)
+    quick_sort_recursion(array, pivot_idx+1, end)
+
+
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+
+    return quick_sort_recursion(array, begin, end)
